2c10ee70f56f7993dd135e4608b41f09df15a8ee
Prash <prash@pelucid.com>
2024-02-01 15:56:13 +0000

    update binary search

--- binary_search.py
+++ binary_search.py
@@ -5,25 +5,37 @@ arr = [4, 8, 10, 23, 12, 45, 2, 5, 7, 9]
 arr = sorted(arr)
 
 
-def binary_search(array, val):
-    if not array:
-        return
-    mid = len(array) // 2
-    print("Mid:", mid, "array: ", array)
-    mid_val = array[mid]
-    if val == mid_val:
+def binary_search(array, target, start, end):
+    """Recursive binary search.
+
+    Array - sorted array to search
+    Target - value to find
+    start - start index of array
+    end - end index of array.
+    """
+    if not array or end == len(array) or start == len(array):
+        return -1
+
+    mid_index = (start + end) // 2
+    print("Mid:", mid_index, "target: ", target, "start: ", start, "end: ", end, "array: ", array)
+    mid_val = array[mid_index]
+    if mid_val == target:
         print(mid_val)
-        return True
-    elif len(array) == 1:
-        return False
-    elif val > mid_val:
-        ans = binary_search(array[mid:], val)
+        return mid_index
+    elif target > mid_val:
+        ans = binary_search(array, target, mid_index + 1, end)
     else:
-        ans = binary_search(array[:mid], val)
+        ans = binary_search(array, target, start, mid_index - 1)
     return ans
 
-res = binary_search(arr, 23)
-print(res)
+res = binary_search(arr, 23, 0, len(arr) - 1)
+print("Index found: ", res)
+
+res = binary_search(arr, 45, 0, len(arr) - 1)
+print("Index found: ", res)
+
+res = binary_search(arr, 10, 0, len(arr) - 1)
+print("Index found: ", res)
 
 def binary_search_it(array, val):
     if not array:
